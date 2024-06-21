3e9bf7e158522e1857f7a9ab2debacbfca1f1a9c
foxfly <1697375477@qq.com>
2024-06-21 09:54:46 +0800

    面试

---
+++ interview_guide/JavaScript/demo.js
@@ -0,0 +1,9 @@
+
+// @ts-nocheck
+// 示例1
+const example1 = () => {
+    console.log('============ example1 =================');
+    function doSomething() { }
+    console.log(doSomething.prototype);
+};
+example1();
