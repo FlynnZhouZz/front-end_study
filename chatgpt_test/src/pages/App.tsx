ce5779cd0125c8aa21ff20a94057eb03f77eba79
foxfly <1697375477@qq.com>
2024-07-11 23:18:38 +0800

    调试ChatGPT

--- chatgpt_test/src/pages/App.tsx
+++ chatgpt_test/src/pages/App.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useCallback, useState, useRef } from 'react';
-import { Container, IconButton, List, ListItem, ListItemText } from '@material-ui/core';
+import { Container, IconButton, List, ListItem } from '@material-ui/core';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
 
 import type { ChatProps } from '@/types/components';
 
@@ -11,11 +10,11 @@ import Send from '@/components/send';
 import OpenRouterKey from '@/components/openRouterKey';
 
 import { storageKey } from '@/config';
-import { fetch } from '@/utils/openai';
-// import { sleep } from '@/utils';
 
 import openaiLogo from '@/assets/openai.png';
 import '@/styles/index.scss';
+
+const URL = 'http://localhost:3001';
 
 function App() {
     const listRef = useRef<HTMLUListElement | null>(null);
@@ -46,54 +45,49 @@ function App() {
         setOpen(!open);
     }, [open]);
 
-    const exec = async (msg: string) => {
+    const exec = useCallback(async (msg: string, close?: () => void) => {
+        const url = `${URL}/chat?msg=${msg}&key=${key}`;
+        const es = new EventSource(url); // 向服务端发起请求
 
-        const response = await axios.get('http://localhost:3000/chat', {
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-            },
-            responseType: 'stream',
-            params: { msg, key },
-        });
-
-        const stream = response.data
-        stream.on('data', (data: any) => {
-            data = data.toString()
-            console.log(data)
-        })
-        return;
-
-        const url = new URL('/chat', '')
-        url.searchParams.set('msg', msg);
-        url.searchParams.set('key', key);
-
-        const es = new EventSource(url) // 向服务端发起请求
-        console.log('ew', es)
         es.onmessage = (event) => {
-            console.log('1231-->', event);
             const data = event.data || '';
             if (data === '[DONE]') {
-                // 结束
-                console.log('done...')
-                es.close()
-                return
+                es.close(); // 结束
+                close?.();
+                return;
             }
 
-            const obj = JSON.parse(data)
-            console.log('obj... ', obj)
+            const obj = JSON.parse(data); // 服务端每次 res.write 返回的信息
 
-            const content = obj.choices[0].delta.content
-            if (content == null) {
-                // 可能因为其他原因停止
-                console.log('stop...')
-                es.close()
-                return
+            const { delta: { content }, finish_reason } = obj.choices[0];
+            console.log(finish_reason, content);
+            if (finish_reason === 'stop') {
+                console.log('stop...');  // 一段回答结束
+                es.close();
+                close?.();
+                return;
             }
 
-            // answerElem.textContent += content
+            setList((list) => {
+                let newHistory;
+                // 如果聊天记录最后一条不是机器人，则拼接一条机器人回答对象
+                if (list[list.length - 1].name !== 'ChatGPT') {
+                    newHistory = [...list, {
+                        type: 2,
+                        name: 'ChatGPT',
+                        msg: content,
+                    }];
+                } else {
+                    // 聊天记录最后一条是机器人,则直接在机器人回答的内容后面拼接新回答
+                    list[list.length - 1].msg = list[list.length - 1].msg + content;
+                    // 不能直接history赋值，要加上[... ]生成新对象,否则setState会认为引用地址没变，不执行页面刷新
+                    // TODO 此处有重复bug
+                    newHistory = [...list];
+                }
+                return newHistory as ChatProps[];
+            });
         }
-    }
+    }, [key]);
     const handleSend = useCallback(async (msg: string, cb?: () => void) => {
         const isExistKey = await getKey();
         if (!isExistKey) return cb?.();
@@ -105,19 +99,8 @@ function App() {
         setList(_list);
 
         // 请求
-        await exec(msg);
-        return;
-        // const chatGptRes = await fetch(msg, key);
-        // if (chatGptRes?.finish_reason === 'stop' && chatGptRes?.message?.content) {
-        //     const _lis: ChatProps[] = [..._list, {
-        //         type: 2,
-        //         name: 'ChatGPT',
-        //         msg: chatGptRes?.message?.content,
-        //     }];
-        //     setList(_lis);
-        // }
-        // cb?.();
-    }, [key, list]);
+        await exec(msg, () => cb?.());
+    }, [exec, list]);
 
     return (
         <Container maxWidth='sm' className='container'>
